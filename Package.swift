ec2da1e7d3393fed73327e5769850a8815ebbab4
Alexy Vinogradov <vinogradov@indoorsnavi.pro>
2024-06-20 15:52:12 +0300

    version 0.1

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .exactItem("5.13.0"))
     ],
 	targets: [
-		.binaryTarget(
-					name: "IndoorsSDK",
-					url: "https://software.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.6.2.zip",
-					checksum: "7c13d917e97cafb560dc0776dae11b4b5f5c29425206c66ee8528f255cbac9be"
-				)
+        .binaryTarget(
+                    name: "IndoorsSDK",
+                    url: "https://software.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.6.2.zip",
+                    checksum: "7c13d917e97cafb560dc0776dae11b4b5f5c29425206c66ee8528f255cbac9be"
+                )
 	]
 )
