ef0814a45811ff08d4cd0842d3907a435883e353
Alexy Vinogradov <vinogradov@indoorsnavi.pro>
2024-07-15 13:05:58 +0300

    Update IndoorsSDK to version 3.6.3

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@
           .binaryTarget(
                       name: "IndoorsSDK",
                       url: "https://software.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.6.3.zip",
-                      checksum: "d197ab4d3ce0c8b6eed11caad7c67fab35096c41ed5c100cbdfbc1b114d66b1d"
+                      checksum: "351a98980d1c9de742c18465bfcb4c7ddf7e12cab83b9a4ee6369657a517871e"
                   )
       ]
   )
