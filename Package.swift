7d837848b50c6a0611cee88aa173dde4851d9aa3
Alexy Vinogradov <vinogradov@indoorsnavi.pro>
2024-11-22 18:07:22 +0300

    Update IndoorsSDK to version 3.6.4

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
   // swift-tools-version: 5.5
 
-  import PackageDescription
+import PackageDescription
 
-  let package = Package(
-      name: "IndoorsSDK",
-      products: [
-          .library(
-              name: "IndoorsSDK",
-              targets: ["IndoorsSDK"]),
-      ],
-      dependencies: [
-                  .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .exactItem("5.13.0"))
-      ],
-      targets: [
-          .binaryTarget(
-                      name: "IndoorsSDK",
-                      url: "https://software.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.6.3.zip",
-                      checksum: "351a98980d1c9de742c18465bfcb4c7ddf7e12cab83b9a4ee6369657a517871e"
-                  )
-      ]
-  )
+let package = Package(
+    name: "IndoorsSDK",
+    products: [
+        .library(
+            name: "IndoorsSDK",
+            targets: ["IndoorsSDK"]),
+    ],
+    dependencies: [
+                .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .exactItem("5.13.0"))
+    ],
+    targets: [
+        .binaryTarget(
+                    name: "IndoorsSDK",
+                    url: "https://software.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.6.4.zip",
+                    checksum: "f0e6c6b0639e058a0b403813927f8c15dab7b900cea500dbfc11a9a59d8d00b3"
+                )
+    ]
+)
